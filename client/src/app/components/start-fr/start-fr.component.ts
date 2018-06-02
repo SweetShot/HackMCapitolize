b1947fe0907614b38e24109d69022522f59e5dcb
Unknown <2015bhavika.shahani@ves.ac.in>
2018-06-02 17:04:12 +0530

    start fundraiser page partially done

---
+++ client/src/app/components/start-fr/start-fr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-fr',
+  templateUrl: './start-fr.component.html',
+  styleUrls: ['./start-fr.component.css']
+})
+export class StartFRComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
