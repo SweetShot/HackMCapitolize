82c2c8610097397c8e4dc1f0899ef1fed095a6bb
Unknown <2015bhavika.shahani@ves.ac.in>
2018-06-02 15:36:15 +0530

    ignore

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
+    RouterModule.forRoot( routes: [
+      {
+        path:'',
+        redirectTo: '/home',
+        pathMatch : 'full'
+      },
+      {
+        path:'/home',
+        component : HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
